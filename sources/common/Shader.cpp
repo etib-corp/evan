eb69320c9e4078fd765d17ae6563c86ea8d8c8a1
Nathan Maillot <nathan.maillot@epitech.eu>
2025-11-04 10:09:25 +0900

    feat(shaders): add raw shader code as member property of shader object (in case of debugging needs)

--- sources/common/Shader.cpp
+++ sources/common/Shader.cpp
@@ -15,6 +15,9 @@ evan::Shader::Shader(const std::vector<uint32_t> &vertexCode, const std::vector<
     createInfo.codeSize = vertexCode.size() * sizeof(uint32_t);
     createInfo.pCode = vertexCode.data();
 
+    _vertexCode = vertexCode;
+    _fragmentCode = fragmentCode;
+
     if (vkCreateShaderModule(device, &createInfo, nullptr, &_vertexShaderModule) != VK_SUCCESS) {
         throw std::runtime_error("failed to create vertex shader module!");
     }
@@ -25,7 +28,6 @@ evan::Shader::Shader(const std::vector<uint32_t> &vertexCode, const std::vector<
     if (vkCreateShaderModule(device, &createInfo, nullptr, &_fragmentShaderModule) != VK_SUCCESS) {
         throw std::runtime_error("failed to create fragment shader module!");
     }
- 
 }
 
 evan::Shader::~Shader()
