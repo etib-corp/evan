ffbdae47ae6dd9ac0c6124f91ddd3a33df8ebfb0
Nathan Maillot <nathan.maillot@epitech.eu>
2026-01-25 21:24:17 +0900

    fix(example): remove while true in hello-world

--- examples/GLFW-hello-world/sources/main.cpp
+++ examples/GLFW-hello-world/sources/main.cpp
@@ -5,8 +5,5 @@ int main(void)
 {
     evan::glfw::Software mySoftware;
     std::cout << "GLFW Software initialized successfully!" << std::endl;
-    while (true) {
-        // Main loop can be implemented here
-    }
     return 0;
 }
