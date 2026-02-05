e3835d81b54a79f4b919f6f6fdaa8abf6987dcc9
Clément Antoine XAVER <clement-antoine.xavier@epitech.eu>
2026-02-05 15:25:26 +0900

    Fix: Fix scripts for installation of LunarG Vulkan SDK

--- scripts/install_vulkan_macos.sh
+++ scripts/install_vulkan_macos.sh
@@ -5,17 +5,7 @@ set -euo pipefail
 base_url=${1:?"Missing base URL"}
 version=${2:?"Missing Vulkan SDK version"}
 
-vulkan_sdk_url="${base_url}${version}/mac/vulkan-sdk-${version}-mac.dmg?Human=true"
-curl -L "$vulkan_sdk_url" -o vulkan-sdk.dmg
+mac/vulkansdk-macos-1.4.335.1.zip
 
-hdiutil attach vulkan-sdk.dmg
-sudo installer -pkg "/Volumes/vulkan-sdk-${version}-mac/vulkan-sdk-${version}-mac.pkg" -target /
-
-export VULKAN_SDK="/usr/local/vulkan-sdk-${version}/macos"
-echo "export VULKAN_SDK=$VULKAN_SDK" >> "$GITHUB_ENV"
-echo "export PATH=$VULKAN_SDK/bin:$PATH" >> "$GITHUB_ENV"
-echo "export DYLD_LIBRARY_PATH=$VULKAN_SDK/lib:$DYLD_LIBRARY_PATH" >> "$GITHUB_ENV"
-echo "export VK_ICD_FILENAMES=$VULKAN_SDK/etc/vulkan/icd.d/MoltenVK_icd.json" >> "$GITHUB_ENV"
-echo "export VK_LAYER_PATH=$VULKAN_SDK/etc/vulkan/explicit_layer.d" >> "$GITHUB_ENV"
-
-hdiutil detach "/Volumes/vulkan-sdk-${version}-mac" || true
+vulkan_sdk_url="${base_url}${version}/mac/vulkansdk-macos-${version}.zip"
+curl -L "$vulkan_sdk_url" -o vulkansdk-macos-${version}.zip
