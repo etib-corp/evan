141873048f0f1d1b0bd9ce52675bfabf0028ac58
Clément Antoine XAVER <clement-antoine.xavier@epitech.eu>
2026-02-05 15:53:30 +0900

    Fix: Fix scripts for installation of LunarG Vulkan SDK

--- scripts/install_vulkan_linux.sh
+++ scripts/install_vulkan_linux.sh
@@ -10,4 +10,4 @@ wget "$vulkan_sdk_url" -O vulkansdk-linux-x86_64-${version}.tar.xz
 
 tar -xf vulkansdk-linux-x86_64-${version}.tar.xz -C vulkansdk-linux-x86_64-${version}
 
-vulkansdk-linux-x86_64-${version}/vulkansdk
+"./vulkansdk-linux-x86_64-${version}/vulkansdk"
