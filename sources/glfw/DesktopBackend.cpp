27094fa21d4efa62130420306d4a8c3d850b62c3
Nathan Maillot <nathan.maillot@epitech.eu>
2026-02-26 18:51:47 +0900

    feat: add desktop back create logical device implementation

--- sources/glfw/DesktopBackend.cpp
+++ sources/glfw/DesktopBackend.cpp
@@ -218,3 +218,63 @@ VkPhysicalDevice evan::DesktopBackend::pickPhysicalDevice(VkInstance instance)
 
 	return physicalDevice;
 }
+
+VkDevice evan::DesktopBackend::createLogicalDevice(VkPhysicalDevice physicalDevice)
+{
+	VkDevice logicalDevice;
+
+	QueueFamilyIndices indices =
+		this->findQueueFamilies(physicalDevice, _surface);
+
+	std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+	std::set<uint32_t> uniqueQueueFamilies = { indices.graphicsFamily.value(),
+											   indices.presentFamily.value() };
+
+	float queuePriority = 1.0f;
+	for (uint32_t queueFamily: uniqueQueueFamilies) {
+		VkDeviceQueueCreateInfo queueCreateInfo {};
+		queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		queueCreateInfo.queueFamilyIndex = queueFamily;
+		queueCreateInfo.queueCount		 = 1;
+		queueCreateInfo.pQueuePriorities = &queuePriority;
+		queueCreateInfos.push_back(queueCreateInfo);
+	}
+
+	VkPhysicalDeviceFeatures deviceFeatures {};
+	deviceFeatures.samplerAnisotropy = VK_TRUE;
+	deviceFeatures.sampleRateShading = VK_TRUE;
+
+	VkDeviceCreateInfo createInfo {};
+	createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+	createInfo.queueCreateInfoCount =
+		static_cast<uint32_t>(queueCreateInfos.size());
+	createInfo.pQueueCreateInfos = queueCreateInfos.data();
+
+	createInfo.pEnabledFeatures = &deviceFeatures;
+
+	createInfo.enabledExtensionCount =
+		static_cast<uint32_t>(deviceExtensions.size());
+	createInfo.ppEnabledExtensionNames = deviceExtensions.data();
+
+	if (enableValidationLayers) {
+		createInfo.enabledLayerCount =
+			static_cast<uint32_t>(validationLayers.size());
+		createInfo.ppEnabledLayerNames = validationLayers.data();
+	} else {
+		createInfo.enabledLayerCount = 0;
+	}
+
+	if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &logicalDevice)
+		!= VK_SUCCESS) {
+		throw std::runtime_error("failed to create logical device!");
+	}
+
+	// TODO: move that in DeviceContext
+	// vkGetDeviceQueue(_logicalDevice, indices.graphicsFamily.value(), 0,
+	// 				 &_graphicsQueue);
+	// vkGetDeviceQueue(_logicalDevice, indices.presentFamily.value(), 0,
+	// 				 &_presentQueue);
+
+	return logicalDevice;
+}
