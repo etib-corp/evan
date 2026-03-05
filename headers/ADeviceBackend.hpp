4b986e3259372f4503528560a36531ab5ac0b3a7
Nathan Maillot <nathan.maillot@epitech.eu>
2026-03-05 16:17:43 +0900

    refactor: move queue family indices out of device backend

--- headers/ADeviceBackend.hpp
+++ headers/ADeviceBackend.hpp
@@ -11,8 +11,8 @@
 #include "Version.hpp"
 
 #include "IPlatform.hpp"
+#include "QueueFamilyIndices.hpp"
 
-#include <optional>
 #include <set>
 
 namespace evan
@@ -46,42 +46,6 @@ namespace evan
 		 * Mailbox, etc.).
 		 */
 		std::vector<VkPresentModeKHR> presentModes;
-	};
-
-	/**
-	 * @brief A structure to represent Vulkan queue family indices.
-	 *
-	 * This structure is used to store the indices of queue families
-	 * that support specific operations, such as graphics and presentation.
-	 *
-	 */
-	struct QueueFamilyIndices {
-		/*
-		 * An optional value representing the index of the queue family
-		 * that supports graphics operations.
-		 */
-		std::optional<uint32_t> graphicsFamily;
-
-		/*
-		 * An optional value representing the index of the queue family
-		 * that supports presentation operations.
-		 */
-		std::optional<uint32_t> presentFamily;
-
-		/*
-		 * @brief isComplete
-		 *
-		 * A method to check if both graphicsFamily and presentFamily
-		 * have been assigned values.
-		 *
-		 * @return true if both graphicsFamily and presentFamily have
-		 * values, indicating that the required queue families are
-		 * available. False otherwise.
-		 */
-		bool isComplete() const
-		{
-			return graphicsFamily.has_value() && presentFamily.has_value();
-		}
 	};
 
 	class ADeviceBackend
