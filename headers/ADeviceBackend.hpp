06185c3cae5f5be4564f06cbea9c7ba2b164919a
Nathan Maillot <nathan.maillot@epitech.eu>
2026-03-10 16:07:03 +0900

    feat: add additional utils methods (related to the device) into abstract device backend

--- headers/ADeviceBackend.hpp
+++ headers/ADeviceBackend.hpp
@@ -20,6 +20,120 @@ namespace evan
 	class ADeviceBackend
 	{
 		public:
+		/**
+		 * @struct CreateImageProperties
+		 * @brief Encapsulates the properties required to create a Vulkan image
+		 * and allocate its memory.
+		 *
+		 * This structure holds all necessary parameters for creating a VkImage
+		 * object and allocating its associated device memory in Vulkan. It
+		 * includes device handles, image dimensions, format, usage flags, and
+		 * references to the resulting image and memory objects.
+		 *
+		 */
+		struct CreateImageProperties {
+			/*
+			 * @brief The Vulkan logical device used for image creation and
+			 * memory allocation.
+			 */
+			VkDevice _logicalDevice;
+			/*
+			 * @brief The Vulkan physical device used to determine memory
+			 * properties.
+			 */
+			VkPhysicalDevice _physicalDevice;
+			/*
+			 * @brief The width of the image in pixels.
+			 */
+			uint32_t _width;
+			/*
+			 * @brief The height of the image in pixels.
+			 */
+			uint32_t _height;
+			/*
+			 * @brief The number of mipmap levels for the image.
+			 */
+			uint32_t _mipLevels;
+			/*
+			 * @brief The number of samples for multisample anti-aliasing
+			 * (MSAA).
+			 */
+			VkSampleCountFlagBits _numSamples;
+			/*
+			 * @brief The format of the image (e.g., VK_FORMAT_R8G8B8A8_SRGB).
+			 */
+			VkFormat _format;
+			/*
+			 * @brief The tiling arrangement of the image (e.g.,
+			 * VK_IMAGE_TILING_OPTIMAL).
+			 */
+			VkImageTiling _tiling;
+			/*
+			 * @brief The usage flags for the image (e.g.,
+			 * VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT).
+			 */
+			VkImageUsageFlags _usage;
+			/*
+			 * @brief The memory property flags for the image (e.g.,
+			 * VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT).
+			 */
+			VkMemoryPropertyFlags _properties;
+			/*
+			 * @brief A reference to the VkImage object that will be created.
+			 */
+			VkImage &_image;
+			/*
+			 * @brief A reference to the VkDeviceMemory object that will be
+			 * allocated for the image.
+			 */
+			VkDeviceMemory &_imageMemory;
+		};
+
+		/**
+		 * @brief Properties required for creating a Vulkan buffer and
+		 * allocating its memory.
+		 *
+		 * This structure encapsulates all necessary parameters for buffer
+		 * creation, including device handles, buffer size, usage flags, memory
+		 * properties, and references to the resulting buffer and its memory.
+		 *
+		 */
+		struct CreateBufferProperties {
+			/*
+			 * @brief The Vulkan logical device used for buffer creation and
+			 * memory allocation.
+			 */
+			VkDevice _logicalDevice;
+			/*
+			 * @brief The Vulkan physical device used to determine memory
+			 * properties.
+			 */
+			VkPhysicalDevice _physicalDevice;
+			/*
+			 * @brief The size of the buffer in bytes.
+			 */
+			VkDeviceSize _size;
+			/*
+			 * @brief The usage flags for the buffer (e.g.,
+			 * VK_BUFFER_USAGE_VERTEX_BUFFER_BIT).
+			 */
+			VkBufferUsageFlags _usage;
+			/*
+			 * @brief The memory property flags for the buffer (e.g.,
+			 * VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT).
+			 */
+			VkMemoryPropertyFlags _properties;
+			/*
+			 * @brief A reference to the VkBuffer object that will be created.
+			 */
+			VkBuffer &_buffer;
+			/*
+			 * @brief A reference to the VkDeviceMemory object that will be
+			 * allocated for the buffer.
+			 */
+			VkDeviceMemory &_bufferMemory;
+		};
+
 		virtual ~ADeviceBackend();
 
 		virtual void createInstance(const IPlatform &platform,
@@ -53,6 +167,14 @@ namespace evan
 		 * containing information about each layer.
 		 */
 		std::vector<VkLayerProperties> getAvailableLayers();
+
+		void createBuffer(const CreateBufferProperties &properties) const;
+
+		uint32_t findMemoryType(VkPhysicalDevice physicalDevice,
+											   uint32_t typeFilter,
+											   VkMemoryPropertyFlags properties) const;
+
+		void createImage(const CreateImageProperties &properties) const;
 
 		VkInstance
 			_VkInstance;	/// The Vulkan instance, which is the connection
--- sources/ADeviceBackend.cpp
+++ sources/ADeviceBackend.cpp
@@ -21,3 +21,101 @@ std::vector<VkLayerProperties> evan::ADeviceBackend::getAvailableLayers()
 	vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
 	return availableLayers;
 }
+
+void evan::ADeviceBackend::createBuffer(const CreateBufferProperties &properties) const
+{
+	VkBufferCreateInfo bufferInfo {};
+	bufferInfo.sType	   = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+	bufferInfo.size		   = properties._size;
+	bufferInfo.usage	   = properties._usage;
+	bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+	if (vkCreateBuffer(properties._logicalDevice, &bufferInfo, nullptr,
+					   &properties._buffer)
+		!= VK_SUCCESS) {
+		throw std::runtime_error("failed to create buffer!");
+	}
+
+	VkMemoryRequirements memRequirements;
+	vkGetBufferMemoryRequirements(properties._logicalDevice, properties._buffer,
+								  &memRequirements);
+
+	VkMemoryAllocateInfo allocInfo {};
+	allocInfo.sType			  = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+	allocInfo.allocationSize  = memRequirements.size;
+	allocInfo.memoryTypeIndex = this->findMemoryType(
+		properties._physicalDevice, memRequirements.memoryTypeBits,
+		properties._properties);
+
+	if (vkAllocateMemory(properties._logicalDevice, &allocInfo, nullptr,
+						 &properties._bufferMemory)
+		!= VK_SUCCESS) {
+		throw std::runtime_error("failed to allocate buffer memory!");
+	}
+
+	vkBindBufferMemory(properties._logicalDevice, properties._buffer,
+					   properties._bufferMemory, 0);
+}
+
+uint32_t evan::ADeviceBackend::findMemoryType(VkPhysicalDevice physicalDevice,
+											   uint32_t typeFilter,
+											   VkMemoryPropertyFlags properties) const
+{
+	VkPhysicalDeviceMemoryProperties memProperties;
+
+	vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+	for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+		if ((typeFilter & (1 << i))
+			&& (memProperties.memoryTypes[i].propertyFlags & properties)
+				== properties) {
+			return i;
+		}
+	}
+
+	throw std::runtime_error("Failed to find suitable memory type!");
+}
+
+void evan::ADeviceBackend::createImage(const CreateImageProperties &properties) const
+{
+	VkImageCreateInfo imageInfo {};
+	imageInfo.sType			= VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+	imageInfo.imageType		= VK_IMAGE_TYPE_2D;
+	imageInfo.extent.width	= properties._width;
+	imageInfo.extent.height = properties._height;
+	imageInfo.extent.depth	= 1;
+	imageInfo.mipLevels		= 1;
+	imageInfo.arrayLayers	= 1;
+	imageInfo.format		= properties._format;
+	imageInfo.tiling		= properties._tiling;
+	imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+	imageInfo.usage			= properties._usage;
+	imageInfo.sharingMode	= VK_SHARING_MODE_EXCLUSIVE;
+	imageInfo.mipLevels		= properties._mipLevels;
+	imageInfo.samples		= properties._numSamples;
+
+	if (vkCreateImage(properties._logicalDevice, &imageInfo, nullptr,
+					  &properties._image)
+		!= VK_SUCCESS) {
+		throw std::runtime_error("failed to create image!");
+	}
+
+	VkMemoryRequirements memRequirements;
+	vkGetImageMemoryRequirements(properties._logicalDevice, properties._image,
+								 &memRequirements);
+
+	VkMemoryAllocateInfo allocInfo {};
+	allocInfo.sType			  = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+	allocInfo.allocationSize  = memRequirements.size;
+	allocInfo.memoryTypeIndex = this->findMemoryType(
+		properties._physicalDevice, memRequirements.memoryTypeBits,
+		properties._properties);
+
+	if (vkAllocateMemory(properties._logicalDevice, &allocInfo, nullptr,
+						 &properties._imageMemory)
+		!= VK_SUCCESS) {
+		throw std::runtime_error("Failed to allocate image memory!");
+	}
+
+	vkBindImageMemory(properties._logicalDevice, properties._image,
+					  properties._imageMemory, 0);
+}
