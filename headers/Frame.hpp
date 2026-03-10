a61fd358eaa9bfc3d659f5c361cb610c76651034
Nathan Maillot <nathan.maillot@epitech.eu>
2026-03-10 16:21:56 +0900

    feat: add frame class

---
+++ headers/Frame.hpp
@@ -0,0 +1,47 @@
+/*
+** ETIB PROJECT, 2026
+** evan
+** File description:
+** Frame
+*/
+
+#pragma once
+
+#include "EvanPlatform.hpp"
+
+#include "ADeviceBackend.hpp"
+
+namespace evan {
+    class Frame {
+        public:
+        /**
+         * @brief Uniform buffer object structure.
+         *
+         * This structure represents the data that will be passed to the
+         * vertex shader as a uniform buffer. It contains the model, view,
+         * and projection matrices used for rendering.
+         *
+         */
+        struct UniformBufferObject {
+            glm::mat4 model;	// Model matrix
+            glm::mat4 view;		// View matrix
+            glm::mat4 proj;		// Projection matrix
+        };
+
+        Frame(VkCommandPool commandPool, const ADeviceBackend &deviceBackend);
+        ~Frame();
+
+        void createCommandBuffer(VkDevice device, VkCommandPool commandPool);
+        void createSyncObjects(VkDevice device);
+        void createUniformBuffer(const ADeviceBackend &deviceBackend);
+
+        protected:
+        VkCommandBuffer _commandBuffer;
+        VkSemaphore _image;
+        VkSemaphore _render;
+        VkFence _inFlight;
+        VkBuffer _uniformBuffer;
+        VkDeviceMemory _uniformBufferMemory;
+        void * _uniformBufferMapped;
+    };
+}
