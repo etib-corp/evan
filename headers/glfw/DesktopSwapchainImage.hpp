40ffa0343dfb7307bb9b45d80d056d5d3c679f89
Nathan Maillot <nathan.maillot@epitech.eu>
2026-03-10 14:32:20 +0900

    feat: add desktop swapchain image implementation

---
+++ headers/glfw/DesktopSwapchainImage.hpp
@@ -0,0 +1,30 @@
+/*
+** ETIB PROJECT, 2026
+** evan
+** File description:
+** DesktopSwapchainImage
+*/
+
+#pragma once
+
+#include "ASwapchainImage.hpp"
+
+#include "glfw/DesktopBackend.hpp"
+
+namespace evan {
+    class DesktopSwapchainImage : public ASwapchainImage {
+        public:
+        DesktopSwapchainImage(const DeviceContext &deviceContext, GLFWwindow *window, VkRenderPass renderpass);
+        ~DesktopSwapchainImage();
+
+        private:
+            VkSwapchainKHR _swapchain;
+
+            VkSurfaceFormatKHR chooseSwapSurfaceFormat(
+	            const std::vector<VkSurfaceFormatKHR> &availableFormats);
+            VkPresentModeKHR chooseSwapPresentMode(
+	            const std::vector<VkPresentModeKHR> &availablePresentModes);
+            VkExtent2D chooseSwapExtent(
+	            const VkSurfaceCapabilitiesKHR &capabilities, GLFWwindow *window);
+    };
+}
