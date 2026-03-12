25acfe56a6be2e090a9d2e4485ed8adf4ef0a5d5
Nathan Maillot <nathan.maillot@epitech.eu>
2026-03-12 13:25:21 +0900

    feat: add first implementation of software

--- headers/Software.hpp
+++ headers/Software.hpp
@@ -7,8 +7,23 @@
 
 #pragma once
 
-#include "DeviceContext.hpp"
 #include "Version.hpp"
+
+#include "DeviceContext.hpp"
+#include "Renderer.hpp"
+#include "Scene.hpp"
+#include "ASwapchainContext.hpp"
+#include "IPlatform.hpp"
+
+#ifdef __OPENXR__
+    #include "openxr/XRPlatform.hpp"
+    #include "openxr/XRSwapchainContext.hpp"
+#elif defined(__GLFW__)
+    #include "glfw/DesktopPlatform.hpp"
+    #include "glfw/DesktopSwapchainContext.hpp"
+#else
+    #error "Unsupported platform"
+#endif
 
 #include <string>
 #include <memory>
@@ -31,14 +46,22 @@ namespace evan
     class Software {
         public:
             Software();
+            Software(const std::string &windowName, const uint32_t width, const uint32_t height);
             ~Software();
+
+            void addScene(std::vector<std::string> texturePaths, std::map<std::string, std::vector<Mesh>> meshData);
 
         protected:
             const std::string _engineName = "Evan Engine";                                  /// The name of the engine.
             const std::string _engineVersion = "0.1.0";                                     /// The version of the engine as a string.
             const std::shared_ptr<Version> _version = std::make_shared<Version>(0, 1, 0);   /// The version of the engine as a Version object.
 
-            std::unique_ptr<DeviceContext> _deviceContext;    /// A unique pointer to the DeviceContext, which manages the device-related information and operations for the engine.
+            std::shared_ptr<DeviceContext> _deviceContext;  /// A shared pointer to a DeviceContext object, which manages the Vulkan device and related resources. The DeviceContext is responsible for initializing Vulkan, selecting physical devices, creating logical devices, and managing command pools and queues.
+            std::shared_ptr<Renderer> _renderer;    /// A shared pointer to a Renderer object, which is responsible for rendering scenes. The Renderer manages graphics pipelines, descriptor sets, and other rendering resources needed to draw objects on the screen.
+            std::vector<Scene> _scenes;    /// A vector of Scene objects, representing the different scenes that can be rendered or managed by the engine. Each Scene contains its own set of objects, materials, and other relevant data for rendering.
+            uint32_t _currentScene;    /// An index to keep track of the current scene being rendered or managed. This allows the engine to switch between different scenes as needed.
+            std::shared_ptr<ASwapchainContext> _swapchainContext;  /// A shared pointer to an ASwapchainContext object, which manages the Vulkan swapchain and related resources. The ASwapchainContext is responsible for creating and managing the swapchain, handling presentation, and managing synchronization primitives related to rendering and presentation.
+            std::shared_ptr<IPlatform> _platform;  /// A shared pointer to an IPlatform object, which provides an abstraction layer for platform-specific operations. The IPlatform interface defines methods for window management, input handling, and other platform-dependent functionality, allowing the engine to be portable across different operating systems and platforms.
         private:
     };
 } // namespace evan
--- sources/Software.cpp
+++ sources/Software.cpp
@@ -9,8 +9,55 @@
 
 evan::Software::Software()
 {
+    #ifdef __OPENXR__
+        _platform = std::make_shared<XRPlatform>(); // TODO: Pass the platform data
+        _deviceContext = std::make_shared<DeviceContext>(*_platform);
+        _swapchainContext = std::make_shared<XRSwapchainContext>(*_deviceContext)
+    #elif defined(__GLFW__)
+        _platform = std::make_shared<DesktopPlatform>("Evan app", 800, 600);
+        _deviceContext = std::make_shared<DeviceContext>(*_platform);
+        _swapchainContext = std::make_shared<DesktopSwapchainContext>(*_deviceContext)
+    #else
+        throw std::runtime_error("Unsupported platform");
+    #endif
+    auto device = _deviceContext->getDeviceBackend()->_device;
+
+    _renderer = std::make_shared<Renderer>();
+    _renderer->createGraphicsPipeline(device, _swapchainContext->getRenderPass(), _deviceContext->getMsaaSamples());
+    for (uint32_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
+        _renderer->createFrame(_deviceContext->getCommandPool(), *_deviceContext->getDeviceBackend());
+    }
+    _renderer->createDescriptorPool(device, 1000); // TODO: Change this with the AssetManager when it will be implemented
+}
+
+evan::Software::Software(const std::string &windowName, const uint32_t width, const uint32_t height)
+{
+    #ifdef __OPENXR__
+        _platform = std::make_shared<XRPlatform>(); // TODO: Pass the platform data
+        _deviceContext = std::make_shared<DeviceContext>(*_platform);
+        _swapchainContext = std::make_shared<XRSwapchainContext>(*_deviceContext)
+    #elif defined(__GLFW__)
+        _platform = std::make_shared<DesktopPlatform>(windowName, width, height);
+        _deviceContext = std::make_shared<DeviceContext>(*_platform);
+        _swapchainContext = std::make_shared<DesktopSwapchainContext>(*_deviceContext)
+    #else
+        throw std::runtime_error("Unsupported platform");
+    #endif
+    auto device = _deviceContext->getDeviceBackend()->_device;
+
+    _renderer = std::make_shared<Renderer>();
+    _renderer->createGraphicsPipeline(device, _swapchainContext->getRenderPass(), _deviceContext->getMsaaSamples());
+    for (uint32_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
+        _renderer->createFrame(_deviceContext->getCommandPool(), *_deviceContext->getDeviceBackend());
+    }
+    _renderer->createDescriptorPool(device, 1000); // TODO: Change this with the AssetManager when it will be implemented
 }
 
 evan::Software::~Software()
 {
+}
+
+void evan::Software::addScene(std::vector<std::string> texturePaths, std::map<std::string, std::vector<Mesh>> meshData)
+{
+    _scenes.emplace_back(*_deviceContext, *_renderer, texturePaths, meshData);
 }
