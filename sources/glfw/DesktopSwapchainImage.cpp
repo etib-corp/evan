d1ebc02eaa989f21bf6321b9369882e7b34e044b
Nathan Maillot <nathan.maillot@epitech.eu>
2026-03-17 12:13:52 +0900

    fix: framebuffers leaksc

--- sources/glfw/DesktopSwapchainImage.cpp
+++ sources/glfw/DesktopSwapchainImage.cpp
@@ -107,11 +107,18 @@ void evan::DesktopSwapchainImage::destroy(VkDevice device)
 VkSurfaceFormatKHR evan::DesktopSwapchainImage::chooseSwapSurfaceFormat(
 	const std::vector<VkSurfaceFormatKHR> &availableFormats)
 {
+	constexpr VkFormat kPreferredSwapchainFormats[] = {
+		VK_FORMAT_R8G8B8A8_SRGB, VK_FORMAT_R8G8B8A8_UNORM,
+		VK_FORMAT_B8G8R8A8_SRGB, VK_FORMAT_B8G8R8A8_UNORM
+	};
+
 	for (const auto &availableFormat: availableFormats) {
-		if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB
-			&& availableFormat.colorSpace
-				== VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
-			return availableFormat;
+		for (VkFormat preferredFormat: kPreferredSwapchainFormats) {
+			if (availableFormat.format == preferredFormat
+				&& availableFormat.colorSpace
+					== VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+				return availableFormat;
+			}
 		}
 	}
 	return availableFormats[0];
