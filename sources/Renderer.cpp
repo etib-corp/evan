6cf7452a5ff0c1fcecffa215b81cf9f425c7925c
Nathan Maillot <nathan.maillot@epitech.eu>
2026-03-17 18:58:16 +0900

    feat: integrate processframe in drawFrame method

--- sources/Renderer.cpp
+++ sources/Renderer.cpp
@@ -245,6 +245,7 @@ void evan::Renderer::drawFrame(const DeviceContext &deviceContext, ASwapchainCon
 		auto result = swapchainContext.aquireImage(i, deviceContext.getDeviceBackend()->_device, _frames[_currentFrameIndex]._image, VK_NULL_HANDLE, imageIndex);
 
 		if (result == VK_ERROR_OUT_OF_DATE_KHR) {
+			 // The swapchain is out of date (e.g. the window was resized) and must be recreated.
 			continue;
 		} else if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR) {
 			throw std::runtime_error("Failed to acquire swap chain image!");
@@ -262,12 +263,28 @@ void evan::Renderer::drawFrame(const DeviceContext &deviceContext, ASwapchainCon
 		submitInfo.pCommandBuffers = &_frames[_currentFrameIndex]._commandBuffer;
 		submitInfo.signalSemaphoreCount = 0;
 
+
 		if (vkQueueSubmit(deviceContext.getGraphicsQueue(), 1, &submitInfo, _frames[_currentFrameIndex]._inFlight) != VK_SUCCESS) {
 			throw std::runtime_error("Failed to submit draw command buffer!");
 		}
+
+		VkSemaphore signalSemaphores[] = {_frames[_currentFrameIndex]._render};
+		submitInfo.signalSemaphoreCount = 1;
+		submitInfo.pSignalSemaphores = signalSemaphores;
+
+		VkPresentInfoKHR presentInfo{};
+		presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+
+		presentInfo.waitSemaphoreCount = 1;
+		presentInfo.pWaitSemaphores = signalSemaphores;
+		presentInfo.pImageIndices = &imageIndex;
+
+		if (!deviceContext.getDeviceBackend()->processFrame(presentInfo, *swapchainContext._swapchainImages[i].get())) {
+			// The swapchain is out of date (e.g. the window was resized) and must be recreated.
+			continue;
+		}
 	}
 	_currentFrameIndex = (_currentFrameIndex + 1) % MAX_FRAMES_IN_FLIGHT;
-	
 }
 
 
