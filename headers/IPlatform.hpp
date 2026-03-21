ac4013577d557575fd6c4a196d5cd7cddad07f3c
Nathan Maillot <nathan.maillot@epitech.eu>
2026-03-21 11:36:26 +0900

    docs: add missing documentation on IPlatform

--- headers/IPlatform.hpp
+++ headers/IPlatform.hpp
@@ -12,45 +12,42 @@
 #include <string>
 
 namespace evan {
+    class ADeviceBackend;
+
     /**
      * @brief Abstract interface for platform-specific functionality.
-     * 
+     *
      * Defines a common interface for platform implementations. Derived classes
      * must implement platform-specific behavior.
      */
-    class ADeviceBackend;
-
     class IPlatform {
         public:
             /**
              * @brief Virtual destructor.
-             * 
+             *
              * Ensures proper cleanup of derived class resources.
              */
             virtual ~IPlatform() = default;
-            
+
             /**
              * @brief Get the required instance extensions for the platform.
-             * 
+             *
              * @return A vector of strings representing the required instance extensions.
              */
             virtual std::vector<std::string> getRequiredInstanceExtensions() const = 0;
 
             /**
              * @brief Check if the platform should close.
-             * 
+             *
              * @return true if the platform should close, false otherwise.
              */
             virtual bool shouldClose() const = 0;
 
             /**
              * @brief Poll for platform events.
-             * 
+             *
              * This method should be called regularly to process platform events.
              */
             virtual void pollEvents(ADeviceBackend &deviceBackend) = 0;
-
-        protected:
-
     };
 } // namespace evan
