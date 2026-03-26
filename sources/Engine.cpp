6f9bb4e2c96b17253fc4f64ca3038aead5d4c689
Léo SAUTRON <leo.sautron@epitech.eu>
2026-03-26 22:51:18 +0900

    fix: remove the extern inside the source file

--- sources/Engine.cpp
+++ sources/Engine.cpp
@@ -8,9 +8,9 @@
 #include "Engine.hpp"
 
 #ifdef __ANDROID__
-extern std::unique_ptr<utility::AndroidAssetManager> g_assetManager;
+std::unique_ptr<utility::AndroidAssetManager> g_assetManager;
 #else
-extern std::unique_ptr<utility::AssetManager> g_assetManager;
+std::unique_ptr<utility::AssetManager> g_assetManager;
 #endif
 
 void evan::Engine::initializeAssetManager(void* platformAssetManager) {
