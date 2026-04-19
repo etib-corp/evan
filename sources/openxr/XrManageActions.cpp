d08852542560755969d53f8523f3b7cb4e8dbe15
Léo SAUTRON <leo.sautron@epitech.eu>
2026-04-19 17:56:04 +0900

    feat: add binding for all buttons

--- sources/openxr/XrManageActions.cpp
+++ sources/openxr/XrManageActions.cpp
@@ -86,6 +86,12 @@ void evan::XrManageActions::bindActionSets(XrDeviceBackend &deviceBackend)
         {_handsMotionActions->_handGripAction, InteractionProfile::stringToPath(deviceBackend._XrInstance, "/user/hand/right/input/grip/pose")},
 
         {_manageButtonsActions->_buttonAAction->getAction(), InteractionProfile::stringToPath(deviceBackend._XrInstance, "/user/hand/right/input/a/click")},
+        {_manageButtonsActions->_buttonBAction->getAction(), InteractionProfile::stringToPath(deviceBackend._XrInstance, "/user/hand/right/input/b/click")},
+        {_manageButtonsActions->_buttonXAction->getAction(), InteractionProfile::stringToPath(deviceBackend._XrInstance, "/user/hand/left/input/x/click")},
+        {_manageButtonsActions->_buttonYAction->getAction(), InteractionProfile::stringToPath(deviceBackend._XrInstance, "/user/hand/left/input/y/click")},
+
+        {_manageButtonsActions->_buttonMenuAction->getAction(), InteractionProfile::stringToPath(deviceBackend._XrInstance, "/user/hand/right/input/menu/click")},
+        {_manageButtonsActions->_buttonSystemAction->getAction(), InteractionProfile::stringToPath(deviceBackend._XrInstance, "/user/hand/right/input/system/click")},
     };
 
     XrInteractionProfileSuggestedBinding suggestedBindings{XR_TYPE_INTERACTION_PROFILE_SUGGESTED_BINDING};
