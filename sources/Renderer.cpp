c52a6d4bc13b47bb09f705ce9360b90054205d82
Nathan Maillot <nathan.maillot@epitech.eu>
2026-09-02 10:54:37 +0400

    feat(renderer): add swapchain recreation in renderer

--- sources/Renderer.cpp
+++ sources/Renderer.cpp
@@ -84,8 +84,17 @@ void evan::Renderer::drawFrame(const DeviceContext &deviceContext,
 		return;
 	}
 
-	auto &frame						 = *_frames[_currentFrameIndex];
-	const ViewSet &viewSet			 = swapchainContext.getViewSet();
+	if (swapchainContext.needsSwapchainRecreation()) {
+		this->getLogger().warning()
+			<< "Swapchain marked for recreation (window resized). Recreating "
+			   "swapchain.";
+		swapchainContext.recreateSwapchain(
+			deviceContext, swapchainContext.getRenderPass());
+		return;
+	}
+
+	auto &frame			= *_frames[_currentFrameIndex];
+	const ViewSet &viewSet = swapchainContext.getViewSet();
 	const std::size_t swapchainCount = swapchainContext.getSwapchainCount();
 
 	this->getLogger().info()
