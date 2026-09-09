9805f3c177b84c0c936e46d4344621d26c73ebac
XAVIER Clément Antoine <87516138+clement-antoine-xavier@users.noreply.github.com>
2026-09-09 09:57:45 +0400

    Merge pull request #87 from etib-corp/feat/add-desktop-and-openxr-examples
    feat(examples): add desktop and OpenXR platform examples

---
+++ examples/OpenXR-android-base/app/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    id("com.android.application")
+}
+android {
+    namespace = "com.evan.evan_android"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.evan.evan_android"
+        minSdk = 32
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        externalNativeBuild {
+            cmake {
+                cppFlags += "-std=c++20"
+                arguments.add("-DANDROID_STL=c++_shared")
+                arguments.add("-DANDROID_USE_LEGACY_TOOLCHAIN_FILE=OFF")
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("cpp/CMakeLists.txt")
+            version = "3.31.5"
+        }
+    }
+    sourceSets {
+        getByName("main") {
+            assets.srcDirs("assets")
+            manifest.srcFile("AndroidManifest.xml")
+        }
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
